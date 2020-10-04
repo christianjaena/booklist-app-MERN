4c7906dc42489e3260d75976cb1f197a9e071496
Christian Jaena <christian.jaena@msugensan.edu.ph>
2020-10-04 18:35:21 +0800

    fixed bugs for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
+const morgan = require('morgan');
 const PORT = process.env.PORT || 5000;
 const mongoDBURI = require('./mongoDBConnection');
 const Post = require('./server/Models/PostModel');
@@ -19,10 +20,7 @@ mongoose
 	})
 	.catch(err => console.log(err));
 
-if (process.env.NODE_ENV !== 'production') {
-	const morgan = require('morgan');
-	app.use(morgan('dev'));
-}
+app.use(morgan('dev'));
 app.use(cors());
 app.use(fileUpload());
 app.use(express.json());
